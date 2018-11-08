f49277f0aa3d30a83fa9d12e8c37cd218933a46e
lihao <haol@sparkpad.com>
2018-11-08 14:36:04 +0800

    first running app

--- server.js
+++
@@ -1,1 +0,0 @@
-var http = require("http"); http.createServer(function(request,response) { response.writeHead(200,{"Content-Type": "text/plain"}); response.write("HelloWorld"); response.end(); }).listen(21110);
